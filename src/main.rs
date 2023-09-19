821c86d73a5ccde6622255fce836fe83d8b2e0ae
devinlinux <ssmith969898@gmail.com>
2023-09-18 21:18:34 -0400

    Added simple command line arguments using clap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 mod lexer;
 
 use std::io::{ Write, BufRead };
+use clap::Command;
 
 use crate::lexer::{ Lexer, Token };
 
-fn main() -> std::io::Result<()> {
+
+fn main() {
+    let matches = Command::new("compiler")
+        .about("An experimental compiler")
+        .version("0.4.0")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .author("Michael Bobrowski")
+
+        .subcommand(
+            Command::new("repl")
+            .short_flag('R')
+            .long_flag("repl")
+            .about("Read Evaluate Print Loop")
+        )
+        .subcommand(
+            Command::new("file")
+            .short_flag('F')
+            .long_flag("file")
+            .about("Compile a file")
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("repl", _)) => {
+            repl().expect("Failed to run repl");
+        }
+        _ => {}
+    }
+}
+
+fn repl() -> std::io::Result<()> {
     let stdin = std::io::stdin();
     let mut stdin_lock = stdin.lock();
     
@@ -31,4 +63,8 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn compile_file() {
+
 }
